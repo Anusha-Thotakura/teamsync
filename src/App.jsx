413554c419899fef3abe3c5e8e671512cdb6d018
Anusha Thotakura <anushathotakura666@gmail.com>
2026-02-06 18:32:13 +0530

    Day 1: Initial React Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>TeamSync</h1>
+      <p>
+        Centralized team collaboration platform to reduce miscommunication.
+      </p>
+      <p>
+        <strong>Day 1:</strong> Project setup completed.
+      </p>
+    </div>
+  );
+}
+
+export default App;
